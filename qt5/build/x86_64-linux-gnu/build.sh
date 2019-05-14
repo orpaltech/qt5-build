686de437346b56906038cdeb44f9765a13d6bc4b
orpaltech <ssuloev@orpaltech.com>
2019-05-14 14:55:36 +0300

    Update for new LLVM and BionicBeaver

--- qt5/build/x86_64-linux-gnu/build.sh
+++ qt5/build/x86_64-linux-gnu/build.sh
@@ -29,7 +29,7 @@ QT5_ROOT=$(pwd)
 cd $PWD
 #====================
 QT5_GIT_ROOT=git://code.qt.io/qt
-QT5_BRANCH=${QT5_BRANCH:="5.10"}
+QT5_BRANCH=${QT5_BRANCH:="5.13"}
 QT5_PREFIX=${QT5_PREFIX:="/usr/local/qt-$QT5_BRANCH"}
 QT5_MODULES=("qtxmlpatterns" "qtimageformats" "qtsvg" "qtscript" "qtdeclarative" "qtquickcontrols" "qtquickcontrols2" "qtcharts" "qt3d" "qttranslations" "qttools" "qtmultimedia")
 if [ $BUILD = debug ]; then
@@ -74,8 +74,8 @@ if [ $TARGET != clean ]; then
 
 	sudo apt-get install -y "^libxcb.*" libx11-xcb-dev libglu1-mesa-dev libxrender-dev libgles2-mesa-dev libglfw3-dev libfreetype6-dev libfontconfig1-dev
 	sudo apt-get install -y libedit-dev
-	sudo apt-get install -y flex bison gperf libicu-dev libxslt-dev
-	sudo apt-get install -y libasound2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+	sudo apt-get install -y flex bison gperf libicu-dev libxslt1-dev
+	sudo apt-get install -y libasound2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 	sudo apt-get install -y libjpeg-dev
 fi
 
@@ -86,31 +86,49 @@ if [ $TARGET != clean ]; then
 
         if [ -d $QTBASE_SRC ] && [ -d $QTBASE_SRC/.git ] ; then
 		echo "Update sources for qtbase..."
-                # update sources
-                sudo git -C $QTBASE_SRC fetch
-                sudo git -C $QTBASE_SRC reset --hard
-                sudo git -C $QTBASE_SRC pull
+
+		sudo git -C $QTBASE_SRC fetch origin --tags
+
+		sudo git -C $QTBASE_SRC reset --hard
+		sudo git -C $QTBASE_SRC clean -fd
+
+		echo "Checking out branch: ${QT5_BRANCH}"
+		sudo git -C $QTBASE_SRC checkout -B $QT5_BRANCH origin/$QT5_BRANCH
+		sudo git -C $QTBASE_SRC pull
+
         else
-		echo "Download fresh sources for qtbase..."
-		sudo rm -rf $QTBASE_SRC
-                # clone sources
-                sudo git clone --depth 1 --recursive -b $QT5_BRANCH $QT5_GIT_ROOT/qtbase.git $QTBASE_SRC
+		echo "Clone fresh sources for qtbase..."
+
+		[[ -d $QTBASE_SRC ]] && rm -rf $QTBASE_SRC
+
+		# clone sources
+		sudo git clone $QT5_GIT_ROOT/qtbase.git -b $QT5_BRANCH $QTBASE_SRC
+		[ $? -eq 0 ] || exit $?;
         fi
 
-	for mod in "${QT5_MODULES[@]}" ; do
-                if [ -d $QT5_ROOT/$mod ] && [ -d $QT5_ROOT/$mod/.git ] ; then
-			echo "Update sources for ${mod}..."
-                        # update sources
-                        sudo git -C $QT5_ROOT/$mod fetch
-                        sudo git -C $QT5_ROOT/$mod reset --hard
-                        sudo git -C $QT5_ROOT/$mod pull
-                else
-			echo "Download fresh sources for ${mod}..."
-			sudo rm -rf $QT5_ROOT/$mod
-                        # clone sources
-                        sudo git clone --depth 1 -b $QT5_BRANCH $QT5_GIT_ROOT/$mod.git $QT5_ROOT/$mod
-                fi
+	for MODULE in "${QT5_MODULES[@]}" ; do
+		QT5_MODULE_DIR=${QT5_ROOT}/${MODULE}
+		QT5_MODULE_URL=${QT5_GIT_ROOT}/${MODULE}.git
 
+		if [ -d $QT5_MODULE_DIR ] && [ -d $QT5_MODULE_DIR/.git ] ; then
+			# update sources
+                	sudo git -C $QT5_MODULE_DIR fetch origin --tags
+			sudo git -C $QT5_MODULE_DIR reset --hard
+			sudo git -C $QT5_MODULE_DIR clean -fd
+
+                        echo "Checking out branch: ${QT5_BRANCH}"
+                        sudo git -C $QT5_MODULE_DIR checkout -B $QT5_BRANCH origin/$QT5_BRANCH
+                        sudo git -C $QT5_MODULE_DIR pull
+
+                else
+			echo "Clone fresh sources for ${mod}..."
+
+			[[ -d $QT5_MODULE_DIR ]] && rm -rf $QT5_MODULE_DIR
+
+			# clone sources
+			sudo git clone $QT5_MODULE_URL -b $QT5_BRANCH $QT5_MODULE_DIR
+			[ $? -eq 0 ] || exit $?;
+                fi
         done
 
 fi
@@ -120,16 +138,16 @@ fi
 sudo mkdir -p $QT5_PREFIX
 
 mkdir -p $BUILD_DIR/qtbase
-for mod in "${QT5_MODULES[@]}" ; do
-	mkdir -p $BUILD_DIR/${mod}
+for MODULE in "${QT5_MODULES[@]}" ; do
+	mkdir -p $BUILD_DIR/${MODULE}
 done
 
 if [[ $TARGET = clean ]]; then
 	sudo rm -rf $BUILD_DIR/qtbase/*
 	sudo rm -rf $QT5_ROOT/qtbase/*
-	for mod in "${QT5_MODULES[@]}" ; do
-		sudo rm -rf $BUILD_DIR/${mod}/*
-		sudo rm -rf $QT5_ROOT/${mod}/*
+	for MODULE in "${QT5_MODULES[@]}" ; do
+		sudo rm -rf $BUILD_DIR/${MODULE}/*
+		sudo rm -rf $QT5_ROOT/${MODULE}/*
 	done
 
 	echo "Clean finished."
@@ -141,20 +159,22 @@ fi
 
 cd $BUILD_DIR/qtbase/
 
-$QTBASE_SRC/configure -prefix $QT5_PREFIX $QT5_BUILD \
+$QTBASE_SRC/configure -v -silent \
 			-opensource -confirm-license \
-			-make libs -nomake examples -nomake tests \
-			-v \
-			-nomake examples -nomake tests \
-                        -no-pch \
-                        -no-use-gold-linker \
+			-prefix $QT5_PREFIX \
+			$QT5_BUILD \
+			-make libs \
+			-nomake examples \
+			-nomake tests \
+			-no-pch \
+			-no-use-gold-linker \
 			-opengl es2 \
-                        -no-openssl \
-                        -system-zlib \
-                        -system-libjpeg \
-                        -system-libpng \
-                        -system-freetype \
-                        -no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci -no-sql-odbc -no-sql-psql -no-sql-tds
+			-no-openssl \
+			-system-zlib \
+			-system-libjpeg \
+			-system-libpng \
+			-system-freetype \
+			-no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci -no-sql-odbc -no-sql-psql -no-sql-tds
 
 
 if [ $STOP_ON_CONFIG != no ]; then
@@ -169,9 +189,9 @@ sudo make install
 
 #############################################################################
 
-for mod in "${QT5_MODULES[@]}" ; do
-	cd $BUILD_DIR/${mod}/
-	$QMAKE $QT5_ROOT/${mod}/
+for MODULE in "${QT5_MODULES[@]}" ; do
+	cd $BUILD_DIR/${MODULE}/
+	$QMAKE $QT5_ROOT/${MODULE}/
 
 	make -j 2
 	sudo make install
